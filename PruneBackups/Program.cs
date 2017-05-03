21a28f4ca1b0c9d3ffee86d7154d537bd3b49705
Per Jahn <perjahn@gmail.com>
2017-05-03 15:26:12 +0200

    Added tool for pruning old backups.

---
+++ PruneBackups/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace PruneBackups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args.Length > 0 ? args[0] : ".";
+
+            string[] allfiles = Directory.GetFiles(path)
+                .Where(f => Path.GetFileName(f).Length == 34)
+                .ToArray();
+
+            Console.WriteLine($"Found {allfiles.Length} files.");
+
+            var dategroups = allfiles.GroupBy(f => Path.GetFileName(f).Substring(0, 24));
+            Console.WriteLine($"Found {dategroups.Count()} date groups.");
+            foreach (var group in dategroups.OrderBy(g => g.Key))
+            {
+                string date = group.Key;
+                string[] datefiles = allfiles.Where(f => Path.GetFileName(f).Substring(0, 24) == date).ToArray();
+                Console.WriteLine($"Found {datefiles.Length} files in group '{date}'");
+
+                string keep = datefiles.OrderBy(f => f).Take(1).Single();
+
+                Console.WriteLine($"Keeping file: '{keep}'");
+
+                foreach (string filename in datefiles.OrderBy(f => f).Skip(1))
+                {
+                    Console.WriteLine($"Deleting file: '{filename}'");
+                    // File.Delete(filename);
+                }
+            }
+        }
+    }
+}
---
+++ PruneBackups/PruneBackups.cs
@@ -0,0 +1,48 @@
+using System;
+using System.IO;
+using System.Linq;
+
+namespace PruneBackups
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args.Length > 0 ? args[0]: ".";
+
+            string[] allfiles = Directory.GetFiles(path)
+                .Where(f => Path.GetFileName(f).Length == 34)
+                .ToArray();
+
+            Console.WriteLine($"Found {allfiles.Length} files.");
+            foreach (string f in allfiles)
+            {
+//                Console.WriteLine(f);
+            }
+
+            var dategroups = allfiles.GroupBy(f => Path.GetFileName(f).Substring(0, 24));
+            Console.WriteLine($"Found {dategroups.Count()} date groups.");
+            foreach (var group in dategroups.OrderBy(g => g.Key))
+            {
+                string date = group.Key;
+//                Console.WriteLine(date);  // todo make sure sorted somehow
+                string[] datefiles = allfiles.Where(f => Path.GetFileName(f).Substring(0, 24) == date).ToArray();
+                Console.WriteLine($"Found {datefiles.Length} files in group '{date}'");
+                foreach (string filename in datefiles)
+                {
+//                    Console.WriteLine(filename);
+                }
+
+                string keep = datefiles.OrderBy(f => f).Take(1).Single();
+
+                Console.WriteLine ($"Keeping file: '{keep}'");
+
+                foreach (string filename in datefiles.OrderBy(f => f).Skip(1))
+                {
+                    Console.WriteLine($"Deleting file: '{filename}'");
+                    // File.Delete(filename);
+                }
+            }
+        }
+    }
+}
