74d9aae4f30520425ef161920cf8c88c910f84dc
Per Jahn <perjahn@gmail.com>
2018-03-28 17:13:13 +0200

    Added the best script in the world.

---
+++ GetAzureIPAddresses.ps1
@@ -0,0 +1,22 @@
+# edit with: powershell_ise.exe
+
+Set-StrictMode -v latest
+$ErrorActionPreference = "Stop"
+
+function Main()
+{
+    Login-AzureRmAccount | Out-Null
+    $addresses = @(Get-AzureRmSubscription | % {
+        Write-Host ("Gathering ip addresses from: '" + $_.Name + "'")
+        Set-AzureRmContext -SubscriptionName $_.Name | Out-Null
+        Get-AzureRmPublicIpAddress
+    })
+
+    Write-Host ("Found " + $addresses.Count + " ip addresses.")
+
+    $addresses | select id,ipaddress | ogv
+
+    Read-Host
+}
+
+Main
